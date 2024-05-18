7fe41abf642b720eb5664eafc1df70b042dab64e
Hartaithan <hartaithan@gmail.com>
2024-05-18 17:40:09 +0900

    back to next ¯\_(ツ)_/¯

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import "./globals.css";
+import "@mantine/core/styles.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import type { FC, PropsWithChildren } from "react";
+import AppProviders from "@/providers/AppProviders";
+import { ColorSchemeScript } from "@mantine/core";
+
+const font = Inter({ subsets: ["latin"], fallback: ["Arial"] });
+
+export const metadata: Metadata = {
+  title: "Trophy Hunt Overlay",
+  description: "Overlay for Trophy Hunt streamers",
+};
+
+const MainLayout: FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <html lang="en">
+      <head>
+        <ColorSchemeScript defaultColorScheme="dark" />
+      </head>
+      <body className={font.className}>
+        <AppProviders fontFamily={font.style.fontFamily}>
+          {children}
+        </AppProviders>
+      </body>
+    </html>
+  );
+};
+
+export default MainLayout;
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import type { FC } from "react";
+import { Title } from "@mantine/core";
+
+const HomePage: FC = () => {
+  return <Title>Hello World!</Title>;
+};
+
+export default HomePage;
