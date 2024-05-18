5ccdfcd192fa722ac35a6bc2d3c20a4a6812d0cb
Hartaithan <hartaithan@gmail.com>
2024-05-18 12:34:23 +0900

    install eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,15 +4,37 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+    "alloy",
+    "alloy/react",
+    "alloy/typescript",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "import"],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
     ],
+    "prettier/prettier": ["error"],
+    "no-console": ["error", { allow: ["info", "error"] }],
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
   },
 };
