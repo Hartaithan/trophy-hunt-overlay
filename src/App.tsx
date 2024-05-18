9dc9bac4bbc80487585f230b21f0a50fdbda8105
Hartaithan <hartaithan@gmail.com>
2024-05-18 13:10:39 +0900

    install mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import "@mantine/core/styles.css";
 import type { FC } from "react";
+import { MantineProvider } from "@mantine/core";
+import { theme } from "./styles/theme";
 
 const App: FC = () => {
   return <div>Hello World!</div>;
 };
 
-export default App;
+const Wrapper: FC = () => {
+  return (
+    <MantineProvider defaultColorScheme="dark" theme={theme}>
+      <App />
+    </MantineProvider>
+  );
+};
+
+export default Wrapper;
