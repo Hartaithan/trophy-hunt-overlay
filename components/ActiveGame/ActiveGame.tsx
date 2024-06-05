1f26536cd68012d52c8ef70a551986997222ff4d
Hartaithan <hartaithan@gmail.com>
2024-06-05 10:57:46 +0900

    add basic active game component layout

--- components/ActiveGame/ActiveGame.tsx
+++ components/ActiveGame/ActiveGame.tsx
@@ -1,7 +1,8 @@
-import { Flex } from "@mantine/core";
+import { Flex, Text } from "@mantine/core";
 import type { FC } from "react";
 import classes from "./ActiveGame.module.css";
 import type { FetchGameResponse } from "@/models/game";
+import GameImage from "@/components/GameImage/GameImage";
 
 interface Props {
   activeGame: FetchGameResponse | null;
@@ -9,8 +10,26 @@ interface Props {
 
 const ActiveGame: FC<Props> = (props) => {
   const { activeGame } = props;
+
+  if (!activeGame) {
+    return (
+      <Flex className={classes.container}>
+        <Text>Pick active game!</Text>
+      </Flex>
+    );
+  }
+
   return (
-    <Flex className={classes.container}>active game: {activeGame?.title}</Flex>
+    <Flex className={classes.container}>
+      <GameImage
+        className={classes.image}
+        src={activeGame.thumbnail}
+        name={activeGame.title}
+        isOverlay={activeGame.platforms.includes("PS5")}
+        imageProps={{ fill: true }}
+      />
+      <Text className={classes.title}>{activeGame.title}</Text>
+    </Flex>
   );
 };
 
