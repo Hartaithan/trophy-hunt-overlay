ff7d19ab1c28a80cbdf66d8f275555e5d01b2029
Hartaithan <hartaithan@gmail.com>
2024-06-11 12:02:08 +0900

    add overlay transition on state changes

--- sections/OverlaySection/OverlaySection.tsx
+++ sections/OverlaySection/OverlaySection.tsx
@@ -5,33 +5,54 @@ import type { ActiveGame } from "@/models/game";
 import type { ActiveTrophy } from "@/models/trophy";
 import { ref, onValue } from "firebase/database";
 import { database } from "@/utils/firebase";
-import { Flex } from "@mantine/core";
+import { Flex, Transition } from "@mantine/core";
 
 interface Props {
   userId: string;
 }
 
+const delay = 1000;
+
 const OverlaySection: FC<Props> = (props) => {
   const { userId } = props;
   const [game, setGame] = useState<ActiveGame | null>(null);
   const [trophy, setTrophy] = useState<ActiveTrophy | null>(null);
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    if (visible) return;
+    const timeout = setTimeout(() => setVisible(true), delay);
+    return () => clearTimeout(timeout);
+  }, [visible]);
 
   useEffect(() => {
     onValue(ref(database, userId), (snapshot) => {
       const data = snapshot.val();
-      if (data?.game !== undefined) setGame(data.game);
-      if (data?.trophy !== undefined) setTrophy(data.trophy);
+      setVisible(false);
+      const timeout = setTimeout(() => {
+        if (data?.game !== undefined) setGame(data.game);
+        if (data?.trophy !== undefined) setTrophy(data.trophy);
+        clearTimeout(timeout);
+      }, delay);
     });
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <Flex h="100%" direction="column" justify="center" align="center">
-      <pre style={{ whiteSpace: "pre-wrap" }}>
-        active game {JSON.stringify(game, null, 2)}
-      </pre>
-      <pre style={{ whiteSpace: "pre-wrap" }}>
-        active trophy {JSON.stringify(trophy, null, 2)}
-      </pre>
+      <Transition mounted={visible} keepMounted>
+        {(styles) => (
+          <pre style={{ ...styles, whiteSpace: "pre-wrap" }}>
+            active game {JSON.stringify(game, null, 2)}
+          </pre>
+        )}
+      </Transition>
+      <Transition mounted={visible} keepMounted>
+        {(styles) => (
+          <pre style={{ ...styles, whiteSpace: "pre-wrap" }}>
+            active trophy {JSON.stringify(trophy, null, 2)}
+          </pre>
+        )}
+      </Transition>
     </Flex>
   );
 };
