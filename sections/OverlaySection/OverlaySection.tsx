bf2c0e86d942a9f6d6a3cf746262aeea957b38fe
Hartaithan <hartaithan@gmail.com>
2024-06-29 14:39:46 +0900

    prevent animations if data is not changed

--- sections/OverlaySection/OverlaySection.tsx
+++ sections/OverlaySection/OverlaySection.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useEffect, useState, type FC } from "react";
+import type { Dispatch, MutableRefObject, SetStateAction } from "react";
+import { useEffect, useRef, useState, type FC } from "react";
 import type { ActiveGame } from "@/models/game";
 import type { ActiveTrophy } from "@/models/trophy";
 import { ref, onValue } from "firebase/database";
@@ -21,26 +22,34 @@ type Game = ActiveGame | null;
 type Trophy = ActiveTrophy | null;
 type Data = ActiveGame | ActiveTrophy | null;
 
-const isSame = <T extends Data>(prev: T, next: T): T => {
-  const isSame = prev?.id === next?.id;
-  return isSame ? prev : next;
+const setData = <T extends Data>(
+  data: T,
+  ref: MutableRefObject<T>,
+  setData: Dispatch<SetStateAction<T>>,
+) => {
+  const isSame = ref.current?.id === data?.id;
+  if (isSame) return;
+  setData(null as T);
+  ref.current = null as T;
+  const timeout = setTimeout(() => {
+    setData(data);
+    ref.current = data;
+    clearTimeout(timeout);
+  }, delay);
 };
 
 const OverlaySection: FC<Props> = (props) => {
   const { userId } = props;
   const [game, setGame] = useState<Game>(null);
+  let gameRef = useRef<Game>(null);
   const [trophy, setTrophy] = useState<Trophy>(null);
+  let trophyRef = useRef<Trophy>(null);
 
   useEffect(() => {
     onValue(ref(database, userId), (snapshot) => {
       const data = snapshot.val();
-      setGame(null);
-      setTrophy(null);
-      const timeout = setTimeout(() => {
-        setGame((prev) => isSame<Game>(prev, data.game));
-        setTrophy((prev) => isSame<Trophy>(prev, data.trophy));
-        clearTimeout(timeout);
-      }, delay);
+      setData<Game>(data.game, gameRef, setGame);
+      setData<Trophy>(data.trophy, trophyRef, setTrophy);
     });
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
