29e095e8469928c73fcf174411f0284913cd9428
Hartaithan <hartaithan@gmail.com>
2024-06-29 14:11:18 +0900

    use framer motion for animations

--- sections/OverlaySection/OverlaySection.tsx
+++ sections/OverlaySection/OverlaySection.tsx
@@ -5,8 +5,9 @@ import type { ActiveGame } from "@/models/game";
 import type { ActiveTrophy } from "@/models/trophy";
 import { ref, onValue } from "firebase/database";
 import { database } from "@/utils/firebase";
-import { Flex, Transition } from "@mantine/core";
+import { Flex } from "@mantine/core";
 import classes from "./OverlaySection.module.css";
+import { AnimatePresence, motion } from "framer-motion";
 import OverlayGame from "@/components/OverlayGame/OverlayGame";
 import OverlayTrophy from "@/components/OverlayTrophy/OverlayTrophy";
 
@@ -29,19 +30,12 @@ const OverlaySection: FC<Props> = (props) => {
   const { userId } = props;
   const [game, setGame] = useState<Game>(null);
   const [trophy, setTrophy] = useState<Trophy>(null);
-  // TODO: separate visible states
-  const [visible, setVisible] = useState(true);
-
-  useEffect(() => {
-    if (visible) return;
-    const timeout = setTimeout(() => setVisible(true), delay);
-    return () => clearTimeout(timeout);
-  }, [visible]);
 
   useEffect(() => {
     onValue(ref(database, userId), (snapshot) => {
       const data = snapshot.val();
-      setVisible(false);
+      setGame(null);
+      setTrophy(null);
       const timeout = setTimeout(() => {
         setGame((prev) => isSame<Game>(prev, data.game));
         setTrophy((prev) => isSame<Trophy>(prev, data.trophy));
@@ -52,12 +46,26 @@ const OverlaySection: FC<Props> = (props) => {
 
   return (
     <Flex className={classes.container}>
-      <Transition mounted={visible} keepMounted>
-        {(style) => <OverlayGame game={game} style={style} />}
-      </Transition>
-      <Transition mounted={visible} keepMounted>
-        {(style) => <OverlayTrophy trophy={trophy} style={style} />}
-      </Transition>
+      <AnimatePresence>
+        {game && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}>
+            <OverlayGame game={game} />
+          </motion.div>
+        )}
+      </AnimatePresence>
+      <AnimatePresence>
+        {trophy && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}>
+            <OverlayTrophy trophy={trophy} />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </Flex>
   );
 };
